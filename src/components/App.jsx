12910640ff2c94f8ef53bd736265c504d406b960
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-22 16:07:45 +0200

    added SearchBa

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,32 @@
 
+import SearchBar from './SearchBar/SearchBar';
+import styles from './App.module.css';
+import { useState } from 'react';
+// import clsx from 'clsx';
+// import ErrorMessage from './error-message/ErrorMessage';
+
 function App() {
+
+  const [error] = useState(false);
+  const [filter, setFilter] = useState('');
+  
+  const handleSearch = strFilter => {
+    // setFilter(strFilter);
+    // setItems([]);
+    // setCurrPage(0);
+    // setHasMorePages(false);
+    // updateImages(strFilter, 1);
+  };
+
   return (
-    <div >
+    <div className={styles.container}>
+    <SearchBar onSearch={handleSearch} />
+    {/* <div className={clsx(styles.content, styles.section)}>
+      <ErrorMessage isError={error} />
+    </div> */}
     </div>
   );
 }
+
 
 export default App;
