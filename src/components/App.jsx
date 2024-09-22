fa11d6fc0e51622962715b2a02f0ddd473b4ab99
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-22 16:58:16 +0200

    fetchData added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,55 @@
 import SearchBar from './SearchBar/SearchBar';
 import styles from './App.module.css';
 import { useState } from 'react';
-// import clsx from 'clsx';
-// import ErrorMessage from './error-message/ErrorMessage';
+import { fetchData } from '../api/fetchData';
+import ErrorMessage from './ErrorMessage/ErrorMessage';
+import clsx from 'clsx';
 
 function App() {
-
-  const [error] = useState(false);
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [currPage, setCurrPage] = useState(0);
+  const [hasMorePages, setHasMorePages] = useState(false);
   const [filter, setFilter] = useState('');
-  
+
+  const updateImages = async (strFilter, page) => {
+    try {
+      setError(false);
+      setLoading(true);
+      const data = await fetchData(strFilter, page);
+      if (data.results.length > 0) {
+        console.log(data.results);
+        setItems(prevItems => [...prevItems, ...data.results]);
+        setCurrPage(page);
+        setHasMorePages(page >= data.total_pages ? false : true);
+      } else {
+        setError(true);
+        setHasMorePages(false);
+      }
+    } catch (error) {
+      console.log(error.message);
+      setError(true);
+      setHasMorePages(false);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const handleSearch = strFilter => {
-    // setFilter(strFilter);
-    // setItems([]);
-    // setCurrPage(0);
-    // setHasMorePages(false);
-    // updateImages(strFilter, 1);
+    setFilter(strFilter);
+    setItems([]);
+    setCurrPage(0);
+    setHasMorePages(false);
+    updateImages(strFilter, 1);
   };
 
   return (
     <div className={styles.container}>
     <SearchBar onSearch={handleSearch} />
-    {/* <div className={clsx(styles.content, styles.section)}>
+    <div className={clsx(styles.content, styles.section)}>
       <ErrorMessage isError={error} />
-    </div> */}
+    </div>
     </div>
   );
 }
