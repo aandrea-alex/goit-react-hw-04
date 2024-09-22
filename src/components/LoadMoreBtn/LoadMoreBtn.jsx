2e15b48cf907bcf1dfaa7740cb428400bdd51f98
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-22 18:11:33 +0200

    Update LoadMoreBtn.jsx

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import CustomButton from '../CustomButton/CustomButton';
 import { MOREBTN_CAPTION } from '../../notifications/constants';
 
 const LoadMoreBtn = ({ isVisible, onClick }) => {
   return (
-    <>
+    <React.Fragment>
       {isVisible && (
         <CustomButton onClick={onClick}>{MOREBTN_CAPTION}</CustomButton>
       )}
-    </>
+    </React.Fragment>
   );
 };
 
