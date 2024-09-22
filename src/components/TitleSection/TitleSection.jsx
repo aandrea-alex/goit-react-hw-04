bbc7869d1d26979aacc49f0af3c52d9527425382
aandrea-alex <andrea.alexandrov.67@gmail.com>
2024-09-22 15:08:00 +0200

    initial commit

--- src/components/TitleSection/TitleSection.jsx
+++
@@ -1,7 +0,0 @@
-import styles from './TitleSection.module.css';
-
-const TitleSection = ({ children }) => {
-  return <h2 className={styles.title}>{children}</h2>;
-};
-
-export default TitleSection;
